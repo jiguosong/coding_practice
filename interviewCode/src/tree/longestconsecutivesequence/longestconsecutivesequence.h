5ec9897b67920418d255614b37880434b72c0564
songjiguo <songjiguo@gmail.com>
2016-12-30 20:02:40 -0500

    Test the sync git with another laptop

---
+++ interviewCode/src/tree/longestconsecutivesequence/longestconsecutivesequence.h
@@ -0,0 +1,20 @@
+#ifndef longestconsecutivesequence_H_
+#define longestconsecutivesequence_H_
+
+#include <vector>
+#include <string>
+using std::vector;
+using std::string;
+
+#include "../Tree.h"
+
+class longestconsecutivesequence
+{
+public:
+	void dummy();
+	int longestConsecutive(TreeNode* root);
+private:
+
+};
+
+#endif
